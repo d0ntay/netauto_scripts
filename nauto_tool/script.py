7adaf74af8f06eeed80e29c8f5200337df0248cb
Dante Cicciarelli <dcicciarelli@visionarysolutions.com>
2025-06-12 12:05:12 -0400

    refactored code so that functions are more seperated and serve one purpose each

--- nauto_tool/script.py
+++ nauto_tool/script.py
@@ -22,7 +22,6 @@ def loader(data):
         trim_blocks=True,
         lstrip_blocks=True,
     )
-    
     for device in data['devices']:
         try:
             if device["device_type"] not in TEMPLATE_MAP:
@@ -32,48 +31,74 @@ def loader(data):
             template = env.get_template(template_name)
             rendered = template.render(device=device)
             config_lines = rendered.strip().splitlines()
-            if len(sys.argv) > 1:
-                if sys.argv[1] == "--dry_run":
-                    dry_run(device['name'], config_lines)
-                    continue
-
-            conn = ConnectHandler(
+            net_connect(
+                config_lines,
                 device_type = device["device_type"],
-                host = device["host"],
-                username = USERNAME,
-                password = PASSWORD,
-            )
-            if device["device_type"] == "juniper":
-                juniper_device(device, conn, config_lines)
-            elif device["device_type"] == "cisco_ios":
-                cisco_device(device, conn, config_lines)
+                device_host = device["host"],
+                device_name = device["name"],
+                device_username = USERNAME,
+                device_password = PASSWORD,
+            )           
+                    
         except Exception as e:
             print(f"Error on {device["name"]} | {device["host"]} : {e}")
 
-def juniper_device(device ,conn, config_lines):
+def juniper_device(device_name ,conn, config_lines):
     output = conn.send_config_set(config_lines)
     conn.commit()
     conn.exit_config_mode()
     conn.disconnect()
-    OUTPUTS[device['name']] = output
-    print(f"Finished {device['name']}")
+    OUTPUTS[device_name] = output 
+    print(f"Finished {device_name}")
 
-def cisco_device(device, conn, config_lines):
+def cisco_device(device_name, conn, config_lines):
     conn.enable()
     output = conn.send_config_set(config_lines)
     conn.set_base_prompt()
     conn.save_config()
     conn.disconnect()
-    OUTPUTS[device['name']] = output
-    print(f"Finished {device['name']}")
+    OUTPUTS[device_name] = output
+    print(f"Finished {device_name}")
 
-def dry_run(device, config_lines):
-    for line in config_lines:
-        print(f"{device} - {line}")
+def net_connect(config_lines, device_type, device_host, device_name, device_username, device_password):
+    conn = ConnectHandler(
+        device_type = device_type,
+        host = device_host,
+        username = device_username,
+        password = device_password,
+    )
+    if device_type == "juniper":
+        juniper_device(device_name, conn, config_lines)
+    elif device_type == "cisco_ios":
+        cisco_device(device_name, conn, config_lines)
+
+def dry_run(data):
+    env = Environment(
+            loader=FileSystemLoader("./templates"),
+            trim_blocks=True,
+            lstrip_blocks=True,
+        )
+    for device in data['devices']:
+        try:
+            if device["device_type"] not in TEMPLATE_MAP:
+                print(f"Skipping {device["name"]}, device type not supported : {device["device_type"]}")
+                continue
+            template_name = TEMPLATE_MAP[device["device_type"]]
+            template = env.get_template(template_name)
+            rendered = template.render(device=device)
+            config_lines = rendered.strip().splitlines()
+            for line in config_lines:
+                print(f"{device["name"]} # {line}")
+        except Exception as e:
+            print(e)
+    sys.exit()
 
 def main():
     with open("./inventory/devices.yaml", "r") as f:
         data = yaml.safe_load(f)
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--dry_run":
+            dry_run(data)
     loader(data)
     now = datetime.now()
     timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")

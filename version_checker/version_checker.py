3e0ed4db8d9c48a042784722353a3ebee0ce9201
Dante Cicciarelli <dcicciarelli@visionarysolutions.com>
2025-07-01 07:53:51 -0400

    added threading to version checker

--- version_checker/version_checker.py
+++ version_checker/version_checker.py
@@ -2,33 +2,40 @@ from datetime import datetime
 import yaml
 from netmiko import ConnectHandler
 import getpass
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 USERNAME = input("Username: ")
 PASSWORD = getpass.getpass()
 
 OUTPUTS = {}
 
-def net_connect (data):
-    for device in data["devices"]:
-        conn = ConnectHandler(
-            device_type = device["device_type"],
-            host = device["host"],
-            username = USERNAME,
-            password = PASSWORD,
-        )
-        if device["device_type"] == "juniper":
-            juniper_device(conn, conn.host)
-        elif device["device_type"] == "cisco_ios":
-            cisco_device(conn, conn.host)
+def net_connect (device):
+        try:
+            conn = ConnectHandler(
+                device_type = device["device_type"],
+                host = device["host"],
+                username = USERNAME,
+                password = PASSWORD,
+            )
+            if device["device_type"] == "juniper":
+                juniper_device(conn, conn.host)
+            elif device["device_type"] == "cisco_ios":
+                cisco_device(conn, conn.host)
+        except Exception:
+            print(f'{device["host"]} - Failed')
+
 
 def juniper_device(conn, host):
     output = conn.send_command("show version | match JUNOS")
     generate_output(output, host)
     conn.disconnect()
+    print(f"{host} - Done!")
+
 def cisco_device(conn, host):
     output = conn.send_command("show version | include Version")
     generate_output(output, host)
     conn.disconnect()
+    print(f"{host} - Done!")
 
 def generate_output(output, host):
     final_output = output.strip().splitlines()[0]
@@ -52,8 +59,13 @@ def create_output():
 def main():
     with open("./inventory/devices.yaml", "r") as f:
         data = yaml.safe_load(f)
-        net_connect(data)
-        create_output()
+    futures = []
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        for device in data["devices"]:
+            futures.append(executor.submit(net_connect, device))
+        for future in as_completed(futures):
+            future.result()
+    create_output()
 
 if __name__ == "__main__":
     main()
